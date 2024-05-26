454a742e1e2f3c9a607ca1835a9c89a24af3c832
Ezeki Zibzibadze <ez@jok.io>
2024-05-26 15:16:38 +0400

    feat: add SSR

---
+++ src/app/app.config.ts
@@ -0,0 +1,23 @@
+import { ApplicationConfig, enableProdMode } from '@angular/core';
+import { provideClientHydration } from '@angular/platform-browser';
+import { RouteReuseStrategy, provideRouter } from '@angular/router';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
+
+import { routes } from './app.routes';
+import { environment } from '../environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular({ mode: 'ios' }),
+    provideClientHydration(),
+    provideRouter(routes),
+  ],
+};
